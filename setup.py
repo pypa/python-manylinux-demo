920bdaf767f8a30ecdd02c34c869928345caa24d
Robert McGibbon <rmcgibbo@gmail.com>
2016-02-01 13:57:50 -0800

    merge .travis.yml

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup, Extension
 extension_module = Extension(
     'pymanylinuxdemo.extension',
      sources=['pymanylinuxdemo/extension.c'],
-     library_dirs=['/usr/lib64/atlas/'],
+     library_dirs=['/usr/lib64/atlas/', '/usr/lib/atlas'],
+     include_dirs=['/usr/include'],
      libraries=['cblas']
 )
 
