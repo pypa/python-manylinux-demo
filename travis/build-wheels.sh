3acb492ab8bdc3d30eb44a8719c37f3d91396e57
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-04-19 10:10:35 -0400

    Update build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,7 +4,7 @@
 yum install -y atlas-devel
 
 # Compile wheels
-for PYBIN in "/opt/python/*/bin/"; do
+for PYBIN in /opt/python/*/bin; do
     ${PYBIN}/pip install -r /io/dev-requirements.txt
     ${PYBIN}/pip wheel /io/ -w wheelhouse/
 done
@@ -15,7 +15,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in "/opt/python/*/bin/"; do
+for PYBIN in /opt/python/*/bin/ do
     ${PYBIN}/pip install python_manylinux_demo --no-index -f wheelhouse
     (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
 done
