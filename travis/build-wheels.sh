1e94799c64d9b8c2ddeecb6bb6147ea4f295629f
Elvis Stansvik <elvstone@gmail.com>
2016-05-09 13:53:12 +0200

    Fix name of distribution
    From the looks of setup.py, it's "python-manylinux-demo" right? (With dashes)

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -17,6 +17,6 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    ${PYBIN}/pip install python_manylinux_demo --no-index -f /io/wheelhouse
+    ${PYBIN}/pip install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
 done
